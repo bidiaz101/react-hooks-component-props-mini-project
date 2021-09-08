77ba30d8cff12267096848bd6e5138340f78ca68
Brian Diaz <bidiaz101@gmail.com>
2021-09-08 13:21:42 -0500

    Added bonus feature

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,27 @@
 import React from "react"
 
-function Article({title, date="January 1, 1970", preview}) {
+function Article({title, date="January 1, 1970", preview, minutes}) {
+    function emojiList(minutes){
+        let interval
+        let emoji
+        let emojis = ""
+        if(minutes<30){
+            interval = 5
+            emoji = "☕️"
+        } else {
+            interval = 10
+            emoji = "🍱"
+        }
+        for(let i=0; i<minutes; i+=interval){
+            emojis += emoji
+        }
+        return emojis
+    }
+
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{date + " | " + emojiList(minutes) + minutes} min read</small>
             <p>{preview}</p>
         </article>
     )
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,7 +4,7 @@ import Article from "./Article"
 function ArticleList({posts}) {
 
     const articleArr = posts.map(postObj => {
-        return <Article key={postObj.id} title={postObj.title} date={postObj.date} preview={postObj.preview} />
+        return <Article key={postObj.id} title={postObj.title} date={postObj.date} preview={postObj.preview} minutes={postObj.minutes} />
     })
 
     return (
